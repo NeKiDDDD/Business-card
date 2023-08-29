7da179b193336762ddf2a4c79112015a8e94a59e
lax3331 <la2life1234@gmail.com>
2023-08-29 16:04:29 +0400

    Initial Commit

---
+++ Business card/Business_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_cardApp.swift
+//  Business card
+//
+//  Created by Nikita Kuznetsov on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Business_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
